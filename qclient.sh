b2be322a48e1c3e446d7d9f67e790899425c1549
CSC466 Server <henry.baxter@gmail.com>
2018-04-02 21:06:56 +0000

    Add QUIC server and client runner

---
+++ qclient.sh
@@ -0,0 +1,1 @@
+/home/ubuntu/chromium/src/out/Default/quic_client --host=127.0.0.1 --port=6121 https://csc466-server.baxtergroup.io
---
+++ qserver.sh
@@ -0,0 +1,4 @@
+/home/ubuntu/chromium/src/out/Default/quic_server \
+  --quic_response_cache_dir=/home/ubuntu/csc466/quic-root \
+  --certificate_file=/home/ubuntu/chromium/src/net/tools/quic/certs/out/leaf_cert.pem \
+  --key_file=/home/ubuntu/chromium/src/net/tools/quic/certs/out/leaf_cert.pkcs8
