01c9f5a393a6d20fe58f1c46e2c71e4b7250264c
CSC466 Server <henry.baxter@gmail.com>
2018-04-02 23:31:52 +0000

    Switch to router dns

--- qclient.sh
+++ qclient.sh
@@ -1,1 +1,3 @@
-/home/ubuntu/chromium/src/out/Default/quic_client --host=127.0.0.1 --port=6121 https://csc466-server.baxtergroup.io
+#!/bin/bash -x
+
+/home/ubuntu/chromium/src/out/Default/quic_client --host=127.0.0.1 --port=6121 https://csc466-router.baxtergroup.io/$1
